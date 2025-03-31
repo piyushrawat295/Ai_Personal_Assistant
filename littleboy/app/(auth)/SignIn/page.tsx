fdd9dfc784f9a8c3739257915fb9bcdfa0ea629c
Piyush Rawat <unexpected086@gmail.com>
2025-03-31 11:14:27 +0530

    Day 2 AuthProvider

---
+++ littleboy/app/(auth)/SignIn/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useGoogleLogin } from "@react-oauth/google";
+import Image from "next/image";
+import React, { useContext } from "react";
+import { MailOpen } from "lucide-react";
+import { GetAuthUserData } from "@/services/GlobalApi";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { AuthContext } from "@/context/AuthContext";
+
+function SignIn() {
+    const CreateUser = useMutation(api.users.CreateUser);
+    const {user,setUser} = useContext(AuthContext);
+  const googleLogin = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      console.log(tokenResponse);
+      if (typeof window !== "undefined") {
+      localStorage.setItem('user_token', tokenResponse.access_token);
+      }
+      const user = await GetAuthUserData(tokenResponse.access_token);
+      console.log(user);
+      const result = await CreateUser({
+        name:user?.name,
+        email: user?.email,
+        picture: user?.picture
+        
+      });
+      console.log("--",result);
+      setUser(result);
+    },
+    onError: (errorResponse) => console.log(errorResponse),
+  });
+  return (
+    <div className="p-20 flex items-center flex-col justify-center h-screen">
+      <div className="gap-10 border rounded-2xl p-15 shadow-md">
+        <Image src="/logo.svg" alt="Logo" width={100} height={100} />
+        <h2 className="m-5 text-2xl">Sign In to AI Personal Assistant</h2>
+        <Button onClick={() => googleLogin()} className="bg-black text-white">
+          <MailOpen /> Login with Email
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
--- littleboy/app/provider.tsx
+++ littleboy/app/provider.tsx
@@ -1,13 +1,25 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ThemeProvider } from "next-themes";
+import { AuthContext } from "@/context/AuthContext";
 
 function Provider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState(null);
+  const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
   return (
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-      {children}
-    </ThemeProvider>
+    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+      <ConvexProvider client={convex}>
+        <AuthContext.Provider value={{ user, setUser }}>
+          <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+            {children}
+          </ThemeProvider>
+        </AuthContext.Provider>
+      </ConvexProvider>
+    </GoogleOAuthProvider>
   );
 }
 
